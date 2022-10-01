907532ba686c08f78532942e2009668d4d25168b
syqsmyl <gemluo@outlook.com>
2022-10-01 15:52:50 +0800

    添加了app3刷新后保持状态的功能

--- dist/main.e73be8c9.js
+++ dist/main.e73be8c9.js
@@ -11383,9 +11383,28 @@ require("./app3.css");
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var $square = (0, _jquery2.default)("#app3 .square");
+var localKey = "app3.index";
+// // yes / no / undefined（默认是undefined）
+var active = localStorage.getItem(localKey) === "yes";
+
+// if (active) {
+//   $square.addClass("active");
+// } else {
+//   $square.removeClass("active");
+// }
+// 以上代码可简写为一行代码
+$square.toggleClass("active", active);
 
 $square.on("click", function () {
-  $square.toggleClass("active"); // 点击一下变化，再点击reset
+  // $square.toggleClass("active"); // 点击一下变化，再点击reset
+  // 使用toggleClass我们不清楚什么时候加了active，什么时候没有加。要想刷新后保持其状态使用hasClass判断
+  if ($square.hasClass("active")) {
+    $square.removeClass("active");
+    localStorage.setItem(localKey, "no");
+  } else {
+    $square.addClass("active");
+    localStorage.setItem(localKey, "yes");
+  }
 });
 },{"jquery":"..\\node_modules\\jquery\\dist\\jquery.js","./app3.css":"app3.css"}],"app4.css":[function(require,module,exports) {
 
@@ -11453,7 +11472,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = '' || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + '6565' + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + '8177' + '/');
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
 
--- src/app3.js
+++ src/app3.js
@@ -2,7 +2,26 @@ import $ from "jquery";
 import "./app3.css";
 
 const $square = $("#app3 .square");
+const localKey = "app3.index";
+// // yes / no / undefined（默认是undefined）
+const active = localStorage.getItem(localKey) === "yes";
+
+// if (active) {
+//   $square.addClass("active");
+// } else {
+//   $square.removeClass("active");
+// }
+// 以上代码可简写为一行代码
+$square.toggleClass("active", active);
 
 $square.on("click", () => {
-  $square.toggleClass("active"); // 点击一下变化，再点击reset
+  // $square.toggleClass("active"); // 点击一下变化，再点击reset
+  // 使用toggleClass我们不清楚什么时候加了active，什么时候没有加。要想刷新后保持其状态使用hasClass判断
+  if ($square.hasClass("active")) {
+    $square.removeClass("active");
+    localStorage.setItem(localKey, "no");
+  } else {
+    $square.addClass("active");
+    localStorage.setItem(localKey, "yes");
+  }
 });
